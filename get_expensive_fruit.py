fdc9a117234902559b6b50b899d6424f7cf1b222
OtabekATW <otabektursunov468@gmail.com>
2023-02-22 16:51:13 +0500

    get_expensive_fruit.py

--- get_expensive_fruit.py
+++ get_expensive_fruit.py
@@ -8,6 +8,12 @@ def get_expensive_fruit(data: str) -> str:
         str: name of the most expensive fruit
     """
     # your code here
-    return  
-
-
+    rows = data.split('\n')[1:]
+    price= []
+    for row in rows:
+        price.append(row.split(',')[1])
+    mx = float(price[0])    
+    for exp in price:
+        if mx < float(exp):
+            mx = float(exp) 
+    return mx
