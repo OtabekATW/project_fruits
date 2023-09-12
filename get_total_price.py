e6bf930ecc32a753fb09fea9f5a0cdee3f4e4728
OtabekATW <otabektursunov468@gmail.com>
2023-09-12 15:32:33 +0500

    get_total_price.py

--- get_total_price.py
+++ get_total_price.py
@@ -1,3 +1,4 @@
+import csv
 def get_total_price(data:str)->float:
     """
     This function returns the total price of the fruits
@@ -7,8 +8,20 @@ def get_total_price(data:str)->float:
     returns:
         float: fruits total price
     """
-    rows = data.split('\n')[1:]
-    s = 0
-    for row in rows:
-        s += float(row.split(',')[1])
-    return s
+    # data = open('fruits.csv','r').read()
+    # rows = data.split('\n')[1:]
+    # total = 0
+    # for row in rows:
+    #     total += float(row.split(',')[1])
+    # return total
+
+    f = open('fruits.csv','r')
+    dat = csv.reader(f)
+
+    total = 0
+    for i in list(dat)[1:]:
+        total += float(i[1])
+
+    return total
+print(get_total_price('fruits.csv'))
+    
