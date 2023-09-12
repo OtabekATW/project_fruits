72182dd3ba8ea38f5f6d8d8114ed42ef503f0c82
OtabekATW <otabektursunov468@gmail.com>
2023-09-12 17:37:54 +0500

    get_cheapest_fruit_id.py

--- get_cheapest_fruit_id.py
+++ get_cheapest_fruit_id.py
@@ -10,12 +10,23 @@ def get_cheapest_fruit_id(data: str) -> int:
     """
     # your code here
 
+    # f = open('fruits.csv','r').read()
+    # dat = f.split('\n')[1:]
+
+    # price = []                         not use csv module
+    # for i in dat:
+    #     price.append(float(i.split(',')[1]))
+
+    # mn = min(price)
+    # return price.index(mn) + 2
+
+
     f = open('fruits.csv','r')
     dat = csv.reader(f)
 
     price = []
     for i in list(dat)[1:]:
         price.append(float(i[-1]))
-    a = min(price)
-    return price.index(a) + 2
+    mn = min(price)
+    return price.index(mn) + 2
 print(get_cheapest_fruit_id('fruits.csv'))
